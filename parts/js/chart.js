bc6db426beb21b6e1b82271e2dcacca7a09c400c
victorfu14 <yiwei_fuscz@outlook.com>
2022-11-15 20:38:08 -0500

    update interactive components for math 389

--- parts/js/chart.js
+++ parts/js/chart.js
@@ -1,64 +1,82 @@
 function getChart(data, total) {
-// set the dimensions and margins of the graph
-var margin = 20;
-var $container = $('.outputarea'),
-    width = $container.width(),
-    height = width;
+    // set the dimensions and margins of the graph
+    var margin = 20;
+    var $container = $('.outputarea'),
+        width = $container.width(),
+        height = width;
 
-if (data.length > 50) { width = width * 2 }
-if (data.length > 100) { width = width * 1.5 }
+    minIdx = data.findIndex(val => val > 0)
+    maxIdx = data.length - data.slice().reverse().findIndex(val => val > 0)
 
-// append the svg object to the body of the page
-var chart = d3.select("#chart")
-.append("svg")
-    .attr("width", width + margin * 2)
-    .attr("height", height + margin * 2)
-.append("g")
-    .attr("transform",
-        "translate(" + margin * 2 + "," + margin + ")");
+    var data_plot = []
+    for (let i = minIdx; i < maxIdx; i++) {
+        data_plot.push([i, data[i]])
+    }
+        
+    if (data_plot.length > 50) { width = width * 2 }
+    if (data_plot.length > 100) { width = width * 1.5 }
+
+    // append the svg object to the body of the page
+    var chart = d3.select("#chart")
+    .append("svg")
+        .attr("width", width + margin * 2)
+        .attr("height", height + margin * 2)
+    .append("g")
+        .attr("transform",
+            "translate(" + margin * 2 + "," + margin + ")");
 
 
-const yScale = d3.scaleLinear()
-    .range([height, 0])
-    .domain([0, Math.max(...data)/total]);
+    const yScale = d3.scaleLinear()
+        .range([height, 0])
+        .domain([0, Math.max(...data)/total]);
+    chart.append('g').call(d3.axisLeft(yScale));
 
-chart.append('g').call(d3.axisLeft(yScale));
+    const xScale = d3.scaleBand()
+        .range([0, width])
+        .domain(Array.from({length: maxIdx - minIdx}, (_, i) => i + minIdx))
+        .padding(0.2)
+    chart.append('g')
+        .attr('transform', `translate(0, ${height})`)
+        .call(d3.axisBottom(xScale));
 
-const xScale = d3.scaleBand()
-    .range([0, width])
-    .domain(data.map((item, index) => index))
-    .padding(0.2)
+    chart.selectAll()
+        .data(data_plot)
+        .enter()
+        .append('rect')
+        .attr('x', (item, _) => xScale(item[0]))
+        .attr('y', (item, _) => yScale(item[1] / total))
+        .attr('height', (item, _) => height - yScale(item[1] / total))
+        .attr('width', xScale.bandwidth())
+        .attr('fill', "#5a6978")
 
-chart.append('g')
-    .attr('transform', `translate(0, ${height})`)
-    .call(d3.axisBottom(xScale));
+    chart.selectAll()
+        .data(data_plot)
+        .enter()
+        .append("text")
+        .text((item, _) => item[1])
+        .attr("x", (item, _) => xScale(item[0]) + xScale.bandwidth() / 2)
+        .attr("y", (item, _) => yScale(item[1] / total) - 5)
+        .attr("font-family" , "sans-serif")
+        .attr("font-size" , "14px")
+        .attr("fill" , "black")
+        .attr("text-anchor", "middle");
 
-chart.selectAll()
-    .data(data)
-    .enter()
-    .append('rect')
-    .attr('x', (item, index) => xScale(index))
-    .attr('y', (item, index) => yScale(item/total))
-    .attr('height', (item, index) => height - yScale(item/total))
-    .attr('width', xScale.bandwidth())
-    .attr('fill', "#5a6978")
 
-chart.selectAll()
-    .data(data)
-    .enter()
-    .append("text")
-    .text((item, index) => item)
-    .attr("x", (item, index) => xScale(index) + xScale.bandwidth() / 2)
-    .attr("y", (item, index) => yScale(item/total) - 5)
-    .attr("font-family" , "sans-serif")
-    .attr("font-size" , "14px")
-    .attr("fill" , "black")
-    .attr("text-anchor", "middle");
+    // var tooltip = d3.select("body")
+    //     .append("div")
+    //     .style("position", "absolute")
+    //     .style("z-index", "10")
+    //     .style("visibility", "hidden")
+    //     .style("background", "#000")
+    //     .text("a simple tooltip");
 
-d3.selectAll("rect").on("mouseover", function(){
-    d3.select(this).attr("opacity", .5)
-}).on("mouseout", function(){
-    d3.select(this).attr("opacity", 1)
-});
+    d3.selectAll("rect").on("mouseover", function(d){
+        d3.select(this).attr("opacity", .5)
+        // tooltip.text(d); 
+        // return tooltip.style("visibility", "visible");
+    }).on("mouseout", function(){
+        d3.select(this).attr("opacity", 1)
+        // return tooltip.style("visibility", "hidden");
+    });
 
 }
